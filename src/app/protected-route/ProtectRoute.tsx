e0f71a2dd84c7fb616037ce896410205170dd179
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2024-03-24 00:33:09 +0700

    Revert "test1"
    This reverts commit 21bff127a04073f61ad14ded44c6ee51d8dd5bb1.

--- src/app/protected-route/ProtectRoute.tsx
+++ src/app/protected-route/ProtectRoute.tsx
@@ -2,13 +2,12 @@ import React, {PropsWithChildren} from 'react';
 import Navbar from "@/components/Navbar/Navbar";
 import {cookies} from "next/headers";
 import {keyAuthentication} from "@/Constant/ConstantAuthConfig";
-import LoginPage from "@/app/login/page";
 
 const ProtectRoute = ({children}:PropsWithChildren) => {
     return (
         <div>
             {cookies().get(keyAuthentication.logged)&&<Navbar/>}
-            {cookies().get(keyAuthentication.logged)?children:<LoginPage/>}
+            {children}
         </div>
     );
 };
