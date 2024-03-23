713b1fc8d12f5a72d9335f64065b451b127db7b1
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2024-03-24 00:00:50 +0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,10 @@ export default function RootLayout({
     <html lang="en">
         <ThemeProvider theme={darkThemeCustom}>
             <CssBaseline/>
-            <Head>
-                <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-            </Head>
+            {/*<Head>*/}
+            {/*    <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />*/}
+            {/*    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />*/}
+            {/*</Head>*/}
             <body className={inter.className}>
                 <ProtectRoute>
                     {children}
